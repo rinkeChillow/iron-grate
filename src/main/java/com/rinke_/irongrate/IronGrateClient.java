deb40a8d1138a84bb325285a9fb21a8297f318e9
rinke <mohamedfriaa69@gmail.com>
2025-12-15 12:27:38 +0100

    Initial commit

---
+++ src/main/java/com/rinke_/irongrate/IronGrateClient.java
@@ -0,0 +1,10 @@
+package com.rinke_.irongrate;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IronGrateClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
