a3c675dc9d875bb67ef9e3fc6e37eb65c0a010f5
rinke <mohamedfriaa69@gmail.com>
2025-12-31 11:56:41 +0100

    aded the cutem rendring where the when iron grate meets other sided of itself it dosent render them

--- src/main/java/com/rinke_/irongrate/block/IronGrateBlock.java
+++ src/main/java/com/rinke_/irongrate/block/IronGrateBlock.java
@@ -2,18 +2,50 @@ package com.rinke_.irongrate.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.Direction;
 
 
-public class IronGrateBlock extends Block {
+public class IronGrateBlock extends Block{
+
+    public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 
     public IronGrateBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getDefaultState().with(WATERLOGGED, false));
     }
-
+    // Hide faces when touching another iron grate
     @Override
     public boolean isSideInvisible(BlockState state, BlockState neighborState, Direction direction) {
-        // Hide faces when touching another iron grate
         return neighborState.isOf(this);
+    }
+    // registers the WATERLOGGED state
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(WATERLOGGED);
+    }
+    // tells Minecraft there is water inside the block
+    @Override
+    public FluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED)
+                ? Fluids.WATER.getStill(false)
+                : super.getFluidState(state);
+    }
+    // changes the block state to waterlogged when water is actually placed
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        BlockState state = this.getDefaultState();
+
+        return state.with(
+                WATERLOGGED,
+                context.getWorld()
+                        .getFluidState(context.getBlockPos())
+                        .getFluid() == Fluids.WATER
+        );
     }
 }
