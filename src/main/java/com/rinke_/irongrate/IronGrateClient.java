bfb2b915288570e289f6209d77a2f676eaba64e3
rinke <mohamedfriaa69@gmail.com>
2026-04-07 22:26:29 +0100

    1.21.6

--- src/main/java/com/rinke_/irongrate/IronGrateClient.java
+++ src/main/java/com/rinke_/irongrate/IronGrateClient.java
@@ -1,15 +1,16 @@
 package com.rinke_.irongrate;
 import com.rinke_.irongrate.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.BlockRenderLayer;
 import net.minecraft.client.render.RenderLayer;
 
 public class IronGrateClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(
+        BlockRenderLayerMap.putBlock(
                 ModBlocks.IRON_GRATE,
-                RenderLayer.getCutout()
+                BlockRenderLayer.CUTOUT
         );
 
     }
