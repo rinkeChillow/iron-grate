fa5c719fb2bbc6b8213e84a0ed5fa98581c5ab05
rinke <mohamedfriaa69@gmail.com>
2026-04-07 14:49:40 +0100

    1.21.4

--- src/main/java/com/rinke_/irongrate/block/IronGrateBlock.java
+++ src/main/java/com/rinke_/irongrate/block/IronGrateBlock.java
@@ -12,7 +12,6 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.block.Waterloggable;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.tick.ScheduledTickView;
